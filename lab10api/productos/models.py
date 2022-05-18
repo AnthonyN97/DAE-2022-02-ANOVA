42daba3ae85072979b9dec94f4f10f8b5bf4b598
Anthony Nova <anthony.nova@tecsup.edu.pe>
2022-05-17 20:50:05 -0500

    laboratorio 10

---
+++ lab10api/productos/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.forms import ImageField
+
+# Create your models here.
+class Producto(models.Model):
+
+    nombre = models.CharField(max_length=100)
+    imagen = models.ImageField(upload_to='productos',blank=True,null=True)
+    precio = models.DecimalField(max_digits=8, decimal_places=2)
