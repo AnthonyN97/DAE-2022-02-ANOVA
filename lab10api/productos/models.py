78a62a9d1f9556d4c7d944757b7599731ead3736
Anthony Nova <anthony.nova@tecsup.edu.pe>
2022-05-21 18:47:11 -0500

    nombre de los productos en la tabla admin

--- lab10api/productos/models.py
+++ lab10api/productos/models.py
@@ -7,3 +7,6 @@ class Producto(models.Model):
     nombre = models.CharField(max_length=100)
     imagen = models.ImageField(upload_to='productos',blank=True,null=True)
     precio = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return self.nombre
